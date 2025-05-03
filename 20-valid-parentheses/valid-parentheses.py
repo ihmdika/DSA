3650dc202127fe25e814076c3617cb65828881c5
Abdullah Sowilah <71607876+ihmdika@users.noreply.github.com>
2025-05-03 12:09:44 +0200

    Time: 0 ms (100.00%) | Memory: 17.8 MB (80.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        closings = {"}": "{", "]": "[", ")":"("}
-        for e in s:
-            if e in closings:
-                if stack and stack[-1] == closings[e]:
+        closings = {")":"(", "}":"{", "]":"["}
+
+        for c in s:
+            if c in closings:
+                if stack and stack[-1] == closings[c]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(e)
+                stack.append(c)
         
-        return not stack
+        return True if not stack else False
