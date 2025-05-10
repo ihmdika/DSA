ca544f986c235022455eeb109525c611af799153
Abdullah Sowilah <71607876+ihmdika@users.noreply.github.com>
2025-05-10 14:02:42 +0200

    Time: 12 ms (53.12%) | Memory: 17.9 MB (63.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,6 @@
 class Solution:
 
-    def buildMap(self, s: str) -> dict:
+    def counter(self, s: str) -> dict:
         strToMap = {}
         for i in s:
             if i in strToMap:
@@ -10,4 +10,4 @@ class Solution:
         return strToMap
 
     def isAnagram(self, s: str, t: str) -> bool:
-        return self.buildMap(s) == self.buildMap(t)
+        return self.counter(s) == self.counter(t)
