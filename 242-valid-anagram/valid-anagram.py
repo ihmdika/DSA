29cfa1d4ccefe23d736f31df006bc8f9dd534328
Abdullah Sowilah <71607876+ihmdika@users.noreply.github.com>
2025-05-14 20:23:40 +0200

    Time: 7 ms (89.56%) | Memory: 17.8 MB (63.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,16 +1,5 @@
 class Solution:
-
-    def counter(self, s: str) -> dict:
-        strToMap = {}
-        for i in s:
-            if i in strToMap:
-                strToMap[i] += 1
-            else:
-                strToMap[i] = 1
-        return strToMap
-
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(t) != len(s):
             return False
-        return self.counter(s) == self.counter(t)
-        # return Counter(s) == Counter(t)
+        return Counter(s) == Counter(t)
